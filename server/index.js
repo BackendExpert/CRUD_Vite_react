279c4de27ceeaeb459f10c998b3a6c4cfbb0480a
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-01-28 08:11:55 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- server/index.js
+++ server/index.js
@@ -1,3 +1,11 @@
-import express from 'express';
-import mysql from 'mysql';
-import cors from 'cors';
+import express from 'express'
+import mysql from 'mysql'
+import cors from 'cors'
+
+const app = express()
+app.use(express.json())
+app.use(cors())
+
+app.listen(3030, ()=> {
+    console.log("Server is Running")
+})

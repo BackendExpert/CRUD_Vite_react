76a01d270b93114194cba7d729364cc8991b080a
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-01-28 08:26:55 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- server/index.js
+++ server/index.js
@@ -6,6 +6,40 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
+const db = mysql.createConnection({
+    host: "localhost",
+    username: "root",
+    password: "",
+    database: "react_crud"
+})
+
+app.get('/', (req, res) => {
+    const sql = "SELECT * FROM user_tbl";
+    db.query(sql, (err, data) => {
+        if(err){
+            return res.json({Error: "ERROR"})
+        }
+        return res.json(data)
+    })
+})
+
+app.post('/create', (req, res) => {
+    const sql ="INSERT INTO user_tbl(fname,lname,mobile,nic,create_at) VALUES (?)";
+    const values = [
+        req.body.fname,
+        req.body.lname,
+        req.body.mobile,
+        req.body.nic,
+        req.body.create_at        
+    ]
+    db.query(sql,[values] , (err, data) => {
+        if(err){
+            return res.json({Error: "ERROR"});
+        }
+        return res.json(data)
+    })
+})
+
 app.listen(3030, ()=> {
     console.log("Server is Running")
 })

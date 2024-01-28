3c33d086e0db9f6515f19b81c3dc77712bee1cc4
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-01-28 09:09:16 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- frant-end/src/App.jsx
+++ frant-end/src/App.jsx
@@ -2,11 +2,13 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Users from './Users'
 import CreateUser from './CreateUser'
 import UpdateUser from './UpdateUser'
+import Nav from "./Nav";
 
 function App() {
 
   return (
     <BrowserRouter>
+    <Nav />
       <Routes>
         <Route path='/' element={<Users />} />
         <Route path='/create' element={<CreateUser />} />
--- frant-end/src/Nav.jsx
+++ frant-end/src/Nav.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 const Nav = () => {
   return (
-    <div className='d-flex justify-content-center py-2 shadow-sm'>
+    <div className='d-flex justify-content-center py-2 shadow-sm fs-2 fw-bold'>
         User Managemet System
     </div>
   )

53ca919dab7f27d374b6ddeae03e7fa85a91888d
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-01-28 09:17:14 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- frant-end/src/Users.jsx
+++ frant-end/src/Users.jsx
@@ -10,7 +10,33 @@ const Users = () => {
         .catch(err => console.log(err))
     }, [])
   return (
-    <div>Users</div>
+    <div>
+        <table className="table">
+            <thead>
+                <tr>
+                    <th scope='col'>#</th>
+                    <th scope='col'>First Name</th>
+                    <th scope='col'>Last Name</th>
+                    <th scope='col'>Mobile Number</th>
+                    <th scope='col'>NIC</th>
+                    <th scope='col'>Added Date</th>
+                </tr>
+            </thead>
+            <tbody>
+                {
+                    users.map(user =>
+                        <tr key={user.id}>
+                            <td>{user.id}</td>
+                            <td>{user.fname}</td>
+                            <td>{user.lname}</td>
+                            <td>{user.mobile}</td>
+                            <td>{user.nic}</td>
+                            <td>{user.create_at}</td>
+                        </tr>)
+                }
+            </tbody>
+        </table>
+    </div>
   )
 }
 

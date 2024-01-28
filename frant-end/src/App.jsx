c6d26b7d74a0396f95722e34a2607595fd5cb160
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-01-28 08:43:05 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- frant-end/src/App.jsx
+++ frant-end/src/App.jsx
@@ -1,10 +1,18 @@
 
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import UpdateUser from './UpdateUser'
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Users />}></Route>
+        <Route path='/create' element={<CreateUser />}></Route>
+        <Route path='/update' element={<UpdateUser />}></Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 

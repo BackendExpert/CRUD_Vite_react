4b43f3cbe71304c157d522c7cac10fbc414013d6
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-01-28 09:12:51 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- frant-end/src/Users.jsx
+++ frant-end/src/Users.jsx
@@ -1,6 +1,14 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
 const Users = () => {
+
+    const [users, setUsers] = useState([])
+    useEffect(() => {
+        axios.get('http"//localhost:3030')
+        .then(res => setUsers(res.data))
+        .catch(err => console.log(err))
+    }, [])
   return (
     <div>Users</div>
   )

9586316e16d4496f7f96fb1e40995f39d227fe8d
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-01-28 09:07:14 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

---
+++ frant-end/src/Nav.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <div className='d-flex justify-content-center py-2 shadow-sm'>
+        User Managemet System
+    </div>
+  )
+}
+
+export default Nav
